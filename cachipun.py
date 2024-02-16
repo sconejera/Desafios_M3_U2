922b5f2dd672a1f7044d00989e00d8c32733a178
sconejera <156250630+sconejera@users.noreply.github.com>
2024-02-16 19:06:46 -0300

    Create cachipun.py
    Juego del cachipun

---
+++ cachipun.py
@@ -0,0 +1,76 @@
+print('\n====================================================')
+
+print('           $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+print('           $$$$$$$$$$$$$$$$$$$$$$$$_____$$$$')
+print('           $$$$_____$$$$$$$$$$$$$$$_____$$$$')
+print('           $$$$_____$$$$$$$$$$$$$$$_____$$$$')
+print('           $$$$_____$$____$$$____$$_____$$$$')
+print('           $$$$_____$______$______$_____$$$$')
+print('           $$$$_____$______$______$_____$$$$')
+print('           $$$$_____$____$$$$$$$$$$$$$$$$$$$')
+print('           $$$$_____$___$$___________$$$$$$$')
+print('           $$$$_____$__$$_______________$$$$')
+print('           $$$$__________$$_____________$$$$')
+print('           $$$$___________$$___________$$$$$')
+print('           $$$$___________$$___________$$$$$')
+print('           $$$$___________$$___________$$$$$')
+print('           $$$$___________$$___________$$$$$')
+print('           $$$$_____________$_________$$$$$$')
+print('           $$$$$_____________________$$$$$$$')
+print('           $$$$$$___________________$$$$$$$$')
+print('           $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+
+
+print('\n╔═══════════════════════════════════════════════════╗')
+print('║           ----Piedra Papel o Tijera----           ║')
+print('╚═══════════════════════════════════════════════════╝')
+
+import random
+# Opciones validas
+OPCIONES = ("piedra", "papel", "tijera")
+
+# Funcion para obtener la jugada del computador
+def jugada_computador():
+    return random.choice(OPCIONES)
+
+# Funcion para determinar el ganador
+def ganador(jugada_usuario, jugada_computador):
+    if jugada_usuario == jugada_computador:
+      return "Empate!😑"
+
+    elif jugada_usuario == "piedra":
+        if jugada_computador == "tijera":
+          return "Ganaste!!😁"
+        else:
+          return "Perdiste!😵"
+
+    elif jugada_usuario == "papel":
+        if jugada_computador == "piedra":
+          return "Ganaste!!😁"
+        else:
+          return "Perdiste!😵"
+
+    elif jugada_usuario == "tijera":
+        if jugada_computador == "papel":
+          return "Ganaste!!😁"
+        else:
+          return "Perdiste!😵"
+
+# Obtiene la jugada del usuario
+while True:
+    try:
+        jugada_usuario = input("\nTu jugada: ")
+        if jugada_usuario not in OPCIONES:
+            raise ValueError("Argumento inválido: Debe ser piedra, papel o tijera")
+        break
+    except ValueError as x:
+        print(x)
+
+# Obtiene la jugada del computador
+jugada_computador = jugada_computador()
+
+# Muestro las jugadas y el resultado
+
+print(f"Computador jugó {jugada_computador}")
+print('\n'  ,ganador(jugada_usuario,jugada_computador))
+print('\n====================================================')
